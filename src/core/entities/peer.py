e7cfe99b358eefa7d5acb802325c4bcc0d05b075
Mark <kr_mark@vk.com>
2025-10-26 16:40:54 +0500

    .

--- src/core/entities/peer.py
+++ src/core/entities/peer.py
@@ -2,12 +2,12 @@ class Peer:
     """VPN-конфигурация пользователя ( peer = узел в сети)"""
     def __init__(
         self,
-        peer_id: str,
+        # peer_id: str,
         user_id: int,
         server_id: str,
         config_data: str  # Содержимое конфига (например, .ovpn)
     ):
-        self.peer_id = peer_id
+        # self.peer_id = peer_id
         self.user_id = user_id
         self.server_id = server_id
         self.config_data = config_data
